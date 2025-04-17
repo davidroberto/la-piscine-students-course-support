9fff2fc912c46dfa90d64bf4c00ae7273d4f3c78
David Robert <contact@david-robert.fr>
2025-04-17 13:37:37 +0200

    connect to DB

--- config.php
+++ config.php
@@ -3,3 +3,15 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+function connectToDB() {
+	try {
+		$pdo = new PDO("mysql:host=localhost:8889;dbname=piscine_reservation", "root", "root");
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		return $pdo;
+	} catch(PDOException $e) {
+		throw new Exception("impossible de se connecter à la DB : " . $e->getMessage());
+	}
+}
+
